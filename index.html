3a39dc9b5ac6503e837f50dac18ac964e3e6f68b
Jared <jared.frenzel.77@gmail.com>
2016-09-22 20:58:50 -0500

    changed file fuck up

---
+++ index.html
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+	<title>Eli&semi;s shit website</title>
+</head>
+<body>
+	Your mom is a whore
+</body>
--- index.js
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<head>
-	<title>Eli&semi;s shit website</title>
-</head>
-<body>
-	Your mom is a whore
-</body>
